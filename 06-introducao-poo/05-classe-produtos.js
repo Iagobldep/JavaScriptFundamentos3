e685d41c611d5389fdcccba9cc98b2ca8d597aaa
Iago <iagobldep@gmail.com>
2026-08-20 16:21:37 -0300

    Introdução POO

--- 06-introducao-poo/05-classe-produtos.js
+++ 06-introducao-poo/05-classe-produtos.js
@@ -1,18 +1,23 @@
 class Produto {
-    constructor(nome, preco){
+    constructor(nome, preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    aplicarDesconto(percentual){
+    aplicarDesconto(percentual) {
         this.preco -= this.preco * (percentual / 100);
+        this.preco = this.preco.toFixed(2);
     }
 
-    valorAPagar(){
+    valorAPagar() {
         console.log(`Valor a pagar = R$${this.preco}`)
     }
 }
 
 const produto = new Produto("Mouse", 100);
-produto.aplicarDesconto(10);
 produto.valorAPagar();
+
+for (let i = 0; i <= 29; i++) {
+    produto.aplicarDesconto(1);
+    produto.valorAPagar();
+}
