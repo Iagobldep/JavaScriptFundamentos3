ca45e6f050bbb921395dc1e23b3e33651db8b478
Iago <iagobldep@gmail.com>
2026-08-20 16:14:08 -0300

    Introdução POO

---
+++ 06-introducao-poo/03-instancias.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    };
+}
+
+const aluno1 = new Pessoa("Iago", 16);
+const aluno2 = new Pessoa("Guilherme", 17);
+console.log(aluno1);
+console.log(aluno2);
---
+++ 06-introducao-poo/04-metodos.js
@@ -0,0 +1,15 @@
+class Pessoa {
+    constructor(nome, nota){
+        this.nome = nome;
+        this.nota = nota;
+    };
+
+    apresentar(){
+        console.log(`${this.nome} - Nota ${this.nota}`)
+    }
+}
+
+const aluno1 = new Pessoa("Iago", 10);
+const aluno2 = new Pessoa("Guilherme", "ZEEEEEEEEEEERO, BÉEEEEEEEEEEEEEE");
+aluno1.apresentar();
+aluno2.apresentar();
