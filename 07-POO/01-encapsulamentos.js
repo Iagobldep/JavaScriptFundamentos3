56e32bb69c14f425c6631fc4c374f2a642599856
Iago <iagobldep@gmail.com>
2026-08-20 16:50:45 -0300

    POO - Não finalizado

---
+++ 07-POO/01-encapsulamentos.js
@@ -0,0 +1,19 @@
+class ContaBancaria {
+    #saldo = 0;
+
+    depositar(valor) {
+        if (valor > 0) this.#saldo += valor;
+    }
+
+    consultaSaldo() {
+        return this.#saldo;
+    }
+}
+
+const conta = new ContaBancaria();
+conta.depositar(100)
+console.log(conta.consultaSaldo());
+conta.depositar(100)
+console.log(conta.consultaSaldo());
+conta.depositar(100)
+console.log(conta.consultaSaldo());
